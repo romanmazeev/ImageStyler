00d410d2393116156e613e09f5183de418e35093
Roman Mazeev <r.mazeev@redmadrobot.com>
2020-03-02 16:16:44 +0300

    Add model creation script

---
+++ Model/model_creator.py
@@ -0,0 +1,20 @@
+import turicreate as tc
+
+# Load the style and content images
+styles = tc.load_images('Dataset/style/')
+content = tc.load_images('Dataset/content/')
+
+# Create a StyleTransfer model
+model = tc.style_transfer.create(styles, content)
+
+# Load some test images
+test_images = tc.load_images('test/')
+
+# Stylize the test images
+stylized_images = model.stylize(test_images)
+
+# Save the model for later use in Turi Create
+model.save('image-styler.model')
+
+# Export for use in Core ML
+model.export_coreml('ImageStyler.mlmodel')
