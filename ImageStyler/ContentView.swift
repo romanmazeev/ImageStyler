172bc17166fc9d401abe60ab858cf8a0df868aca
Roman Mazeev <r.mazeev@redmadrobot.com>
2020-03-06 14:32:25 +0300

    Add XCode project

---
+++ ImageStyler/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ImageStyler
+//
+//  Created by Roman Mazeev on 06.03.2020.
+//  Copyright © 2020 Roman Mazeev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
