9c61ef3befc5e3a2feea9d92ed14698f010b43a3
Roman Mazeev <r.mazeev@redmadrobot.com>
2020-03-16 19:00:00 +0300

    Add new model with new filters

--- ImageStyler/Service/StylesData.swift
+++ ImageStyler/Service/StylesData.swift
@@ -10,6 +10,10 @@ import SwiftUI
 
 struct StylesData {
     static var styles: [Style] = [
-        Style(id: 0, imageData: UIImage(named: "artDeco")!.jpegData(compressionQuality: 0)!, name: "Art deco")
+        Style(id: 0, imageData: UIImage(named: "vanGogh")!.jpegData(compressionQuality: 0)!, name: "Van Gogh"),
+        Style(id: 1, imageData: UIImage(named: "tsunami")!.jpegData(compressionQuality: 0)!, name: "Tsunami"),
+        Style(id: 2, imageData: UIImage(named: "lsd")!.jpegData(compressionQuality: 0)!, name: "Trippy"),
+        Style(id: 3, imageData: UIImage(named: "barocco")!.jpegData(compressionQuality: 0)!, name: "Barocco"),
+        Style(id: 4, imageData: UIImage(named: "virus")!.jpegData(compressionQuality: 0)!, name: "Virus")
     ]
 }
